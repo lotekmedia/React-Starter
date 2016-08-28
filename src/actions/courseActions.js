e61295b5916c8ce662fff1997e97b01b6a84aee6
lotekmedia <me@johnmann.org>
2016-08-28 12:46:57 -0400

    implemented error handling

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses){
     return { type: types.LOAD_COURSES_SUCCESS, courses: courses};
@@ -17,6 +18,7 @@ export function createCourseSuccess(course){
 
 export function loadCourses(){
     return function(dispatch){
+        dispatch(beginAjaxCall());
         return courseApi.getAllCourses().then(
             courses => {
                 dispatch(loadCoursesSuccess(courses));
@@ -28,10 +30,12 @@ export function loadCourses(){
 
 export function saveCourse(course){
     return function(dispatch, getState) {
+        dispatch(beginAjaxCall());
         return courseApi.saveCourse(course).then(savedCourse => {
             course.id ? dispatch(updateCourseSuccess(savedCourse)) :
             dispatch(createCourseSuccess(savedCourse));
         }).catch(error => {
+            dispatch(ajaxCallError(error));
             throw(error);
         });
     };
