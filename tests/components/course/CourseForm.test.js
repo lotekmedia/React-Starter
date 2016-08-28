91db394834841b58d129df3937a03dd1ca2c8551
lotekmedia <me@johnmann.org>
2016-08-28 13:46:45 -0400

    Moved Tests around and added tests

---
+++ tests/components/course/CourseForm.test.js
@@ -0,0 +1,32 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+import CourseForm from '../../../src/components/course/CourseForm';
+
+function setup(saving) {
+    const props = {
+        course: {}, saving: saving, errors: {},
+        onSave: () => { },
+        onChange: () => { }
+    };
+    return shallow(<CourseForm {...props} />);
+}
+describe('CourseForm via Enzyme', () => {
+    it('render form and h1', () => {
+        const wrapper = setup(false);
+        expect(wrapper.find('form').length).toBe(1);
+        expect(wrapper.find('input').props().value).toBe('Save');
+    });
+
+    it('save button is labeled "Save" when not saving', () => {
+        const wrapper = setup(false);
+        expect(wrapper.find('input').props().value).toBe('Save');
+    });
+
+    it('save button is labeled "Saving..." when saving', () => {
+        const wrapper = setup(true);
+        expect(wrapper.find('input').props().value).toBe('Saving...');
+    });
+
+});
